8c791cf7241544420a1ba9ea797cf0d19ec0842a
nikita stepanov <nist1701@gmail.com>
2023-07-12 01:44:15 +0300

    finish!

--- controllers/users.js
+++ controllers/users.js
@@ -78,7 +78,7 @@ const updateUser = async (req, res, _, data) => {
 
 export const updateProfile = (req, res, _) => {
   const { name, about } = req.body;
-  return updateUser(req, res, next, { name, about });
+  return updateUser(req, res, _, { name, about });
 };
 export const updateAvatar = (req, res, _) => {
   const { avatar } = req.body;
